aa87a3a235b8cfc57dd5f18f8ddafc19d8620471
fortune <legalmody@gmail.com>
2016-08-22 13:09:09 +0100

    implemented a post request

---
+++ search.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+var request = require('request');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('Search for something? ', (answer) => {
+
+    var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+answer+'&limit=10&namespace=0&format=json';
+
+    request.post(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(JSON.parse(response.body), url);
+    }
+})
+
+  
+  rl.close();
+});
